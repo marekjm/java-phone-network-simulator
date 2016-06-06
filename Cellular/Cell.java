d59bb9e59777397e8928f72c541975ded47dfe27
Marek Marecki <marekjm@ozro.pw>
2016-06-06 22:24:40 +0200

    ServerSocket

--- Cellular/Cell.java
+++ Cellular/Cell.java
@@ -1,9 +1,10 @@
 package Cellular;
 
-import java.io.Console;
+import java.io.*;
+import java.net.*;
 
 class Cell {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World (from Cell)!");
 
         Console console = System.console();
@@ -16,7 +17,31 @@ class Cell {
                 System.out.println("");
                 break;
             }
-            System.out.println(input);
+
+            String[] parts = input.split("\\s+");
+            System.out.println(parts.length + " " + java.util.Arrays.toString(parts));
+
+            // we need a command and at least one operand
+            if (parts.length < 2) {
+                continue;
+            }
+
+            String command = parts[0];
+            String operand = (parts.length > 1 ? parts[1] : "");
+
+            ServerSocket ssock = new ServerSocket(new Integer(args[1]));
+            Socket sock = ssock.accept();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+
+            switch (command) {
+                case "connect":
+                    System.out.println("connecting...");
+                    break;
+                default:
+                    System.err.println("error: unknown command: " + command);
+            }
         }
     }
 }
