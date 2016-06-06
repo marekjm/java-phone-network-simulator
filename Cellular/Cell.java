0cfda9e943de483622bf07de37e12bc682b7640c
Marek Marecki <marekjm@ozro.pw>
2016-06-06 21:09:24 +0200

    Cells can read commands from console

--- Cellular/Cell.java
+++ Cellular/Cell.java
@@ -1,7 +1,18 @@
 package Cellular;
 
+import java.io.Console;
+
 class Cell {
     public static void main(String[] args) {
         System.out.println("Hello World (from Cell)!");
+
+        Console console = System.console();
+
+        String command = new String("");
+        while (command != null) {
+            System.out.print("> ");
+            command = console.readLine();
+            System.out.println(command);
+        }
     }
 }
