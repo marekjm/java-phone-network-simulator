2fc1b5c533ed90b4bb2a8364e8a77fe664d392f2
Marek Marecki <marekjm@ozro.pw>
2016-06-10 22:51:37 +0200

    Connection logic moved to base class

--- Cellular/Command.java
+++ Cellular/Command.java
@@ -1,9 +1,34 @@
 package Cellular;
 
 import java.util.*;
+import java.io.*;
+import java.net.*;
 
 class Command {
-    private String[] arguments;
+    protected String[] arguments;
+
+    private Socket _socket = null;
+    private BufferedReader _in = null;
+    private PrintWriter _out = null;
+
+    protected Socket socket() { return _socket; }
+    protected BufferedReader in() { return _in; }
+    protected PrintWriter out() { return _out; }
+
+    protected boolean connect(Integer port) {
+        boolean success = false;
+        try {
+            _socket = new Socket("localhost", port);
+            _in = new BufferedReader(new InputStreamReader(_socket.getInputStream()));
+            _out = new PrintWriter(_socket.getOutputStream(), true);
+            success = true;
+        } catch (UnknownHostException e) {
+            System.out.println("failed to create socket: " + e);
+        } catch (IOException e) {
+            System.out.println("failed to connect: " + e);
+        }
+        return success;
+    }
 
     public void execute(Integer port, String phone_number) {
         System.out.println("unknown command: " + java.util.Arrays.toString(arguments));
--- Cellular/UnregisterCommand.java
+++ Cellular/UnregisterCommand.java
@@ -8,15 +8,8 @@ import Cellular.Command;
 
 class UnregisterCommand extends Cellular.Command {
     public void execute(Integer port, String phone_number) {
-        try {
-            Socket sock = new Socket("localhost", port);
-            BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-            PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
-            out.println("unregister " + phone_number);
-        } catch (UnknownHostException e) {
-            System.out.println(this + ": failed: " + e);
-        } catch (IOException e) {
-            System.out.println(this + ": failed: " + e);
+        if (connect(port)) {
+            out().println("unregister " + phone_number);
         }
     }
 
